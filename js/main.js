1bec4cfd662e682d31a74d4fb6022d0c2a99633d
aleksi2105 <aleksi2105@yandex.ru>
2025-12-30 04:44:33 +0300

    fix: mobile-menu with JS

--- js/main.js
+++ js/main.js
@@ -2,17 +2,33 @@ const navbar = document.querySelector(".navbar")
 const logo = document.querySelector(".logo-svg use")
 const mMenuToggle = document.querySelector(".mobile-menu-toggle")
 const menu = document.querySelector(".mobile-menu")
-window.addEventListener('scroll', () => {
-  if (this.scrollY > 1) {
-    navbar.classList.add("navbar-light");
-    logo.href.baseVal = "img/sprite.svg#logo";
 
-  } else {
-    navbar.classList.remove("navbar-light");
-    logo.href.baseVal = "img/sprite.svg#logo-light";
-  }
+const lightModeOn = (event) => {
+  navbar.classList.add("navbar-light");
+  logo.href.baseVal = "img/sprite.svg#logo";
+}
+const lightModeOff = (event) => {
+  navbar.classList.remove("navbar-light");
+  logo.href.baseVal = "img/sprite.svg#logo-light";
+}
+
+const openMenu = (event) => { //функция открывания меню
+  menu.classList.add('is-open');
+  mMenuToggle.classList.add("close-menu");
+  document.body.style.overflow = "hidden"; //запрещает прокрутку под меню
+  lightModeOn();
+}
+const closeMenu = (event) => { //функция закрывания меню
+  menu.classList.remove('is-open');
+  mMenuToggle.classList.remove("close-menu");
+  document.body.style.overflow = ""; //возвращает прокрутку под меню
+  lightModeOff();
+}
+
+window.addEventListener('scroll', () => {
+  this.scrollY > 1 ? lightModeOn() : lightModeOff();
 });
 mMenuToggle.addEventListener("click", (event) => {
   event.preventDefault();
-  menu.classList.toggle('is-open')
+  menu.classList.contains('is-open') ? closeMenu() : openMenu();
 })
