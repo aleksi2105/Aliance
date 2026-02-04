98f98b673902d3d596ceadfa7eb331291ad4f2a1
aleksi2105 <aleksi2105@yandex.ru>
2026-02-04 03:18:43 +0300

    add: section and part our-production-block

---
+++ template-parts/our-production-block.php
@@ -0,0 +1,51 @@
+<section class="section">
+  <div class="container production-wrapper">
+    <div class="production-description">
+      <div class="separator"></div>
+      <h2 class="section-title">Наше производство</h2>
+      <p class="production-text">Предварительные выводы неутешительны: разбавленное изрядной долей эмпатии, рациональное мышление обеспечивает широкому кругу (специалистов) участие в формировании глубокомысленных рассуждений. Но граница обучения кадров создаёт необходимость включения в производственный план целого ряда внеочередных мероприятий с учётом комплекса кластеризации усилий.</p>
+      <p class="production-text2">Реализация намеченных плановых заданий, а также свежий взгляд на привычные вещи - безусловно открывает новые горизонты для соответствующих условий активизации. Предварительные выводы неутешительны: экономическая повестка сегодняшнего дня требует анализа анализа существующих паттернов поведения.</p>
+      <div class="clients-content">
+       <ul class="clients-list">
+              <li class="clients-list-item">
+                <svg width="30" height="30" class="client-logo">
+                  <use xlink:href="./img/sprite.svg#carwash"></use>
+                </svg>
+                Автомобильная химия
+              </li>
+              <li class="clients-list-item">
+                <svg width="30" height="30" class="client-logo">
+                  <use xlink:href="./img/sprite.svg#home"></use>
+                </svg>
+                Бытовая химия
+              </li>
+              <li class="clients-list-item">
+                <svg width="30" height="30" class="clients-list-icon">
+                  <use xlink:href="./img/sprite.svg#shieldAl"></use>
+                </svg>
+                Дезинфицирующие средства
+              </li>
+              <li class="clients-list-item">
+                <svg width="30" height="30" class="clients-list-icon">
+                  <use xlink:href="./img/sprite.svg#aerozol"></use>
+                </svg>
+                Пищевые аэрозоли
+              </li>
+              <li class="clients-list-item">
+                <svg width="30" height="30" class="clients-list-icon">
+                  <use xlink:href="./img/sprite.svg#cosmetic"></use>
+                </svg>
+                Косметическая продукция
+              </li>
+              <li class="clients-list-item">
+                <svg width="30" height="30" class="clients-list-icon">
+                  <use xlink:href="./img/sprite.svg#brush"></use>
+                </svg>
+                Краски аэрозольные
+              </li>              
+         </ul>
+      </div>
+    </div>
+    <img src="img\laboratory.png" alt="laboratory assistant" class="production-lab-img">
+  </div>
+ </section>
