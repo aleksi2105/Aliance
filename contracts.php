d058e96d20fbeb72069772c09f45262bf9972189
aleksi2105 <aleksi2105@yandex.ru>
2026-02-07 02:29:36 +0300

    add: contracts-page.php

--- contracts.php
+++ contracts.php
@@ -3,4 +3,17 @@ $page_title = 'Контрактное производство';
 $header_style = 'header-bg-grey';
 include_once('template-parts\header-page.php');
 include_once('template-parts\contracts-production-block.php');?>
+<section class="section seo">
+  <div class="container">
+    <h3 class="seo-title">SEO Заголовок</h3>
+    <p class="seo-text">Также как дальнейшее развитие различных форм деятельности предполагает независимые способы реализации существующих финансовых и административных условий. Каждый из нас понимает очевидную вещь: курс на социально-ориентированный национальный проект играет важную роль в формировании как самодостаточных, так и внешне зависимых концептуальных решений. Внезапно, интерактивные прототипы набирают популярность среди определенных слоев населения, а значит, должны быть...</p>
+    <div class="seo-button-group">
+    <a href="" class="arrow-load"><svg  width="24" height="24">
+      <use xlink:href="./img/sprite.svg#arrow-load"></use>
+    </svg></a>
+    <a href="#" class="button-link-more">Читать больше</a>
+    </div>
+  </div>
+  
+</section>
 <?php include_once('template-parts\footer.php');?>
