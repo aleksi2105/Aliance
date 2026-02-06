3b1074e20976e1e9e2931f527cf42d673f8f82fa
aleksi2105 <aleksi2105@yandex.ru>
2026-02-06 03:25:47 +0300

    add: clients-block

--- about.php
+++ about.php
@@ -20,6 +20,8 @@ include_once('template-parts\header-page.php');?>
  <!-- /.section production -->
 <?php include_once('template-parts\founder-block.php');?>
 <!-- /.section-founder -->
- <?php include_once('template-parts\research-block.php');?>
+<?php include_once('template-parts\research-block.php');?>
 <!-- /.section-research -->
+<?php include_once('template-parts\clients-block.php');?>
+<!-- /.section-clients -->
 <?php include_once('template-parts\footer.php');?>
