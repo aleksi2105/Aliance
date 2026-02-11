fee27051552f1f031945a2128944aadaa3fa0a12
aleksi2105 <aleksi2105@yandex.ru>
2026-02-11 03:42:23 +0300

    add: blog-more page

--- blog-more.php
+++ blog-more.php
@@ -2,4 +2,145 @@
 $page_title = 'Современная методология разработки одухотворила всех причастных';
 $header_style = 'header-page-background';
 include_once('template-parts\header-page.php');?>
+
+<section class="section blog-text">
+  <div class="container">
+    <div class="blog-text-wrapper">
+      <div class="blog-text-social">
+        <h5 class="blog-text-social-title">Поделиться:</h5>
+        <ul class="social-link-group">
+          <a href="#" class="social-link-item">
+            <svg class="button-icon" width="24" height="24">
+              <use xlink:href="./img/sprite.svg#vk"></use>
+            </svg>
+          </a>
+          <a href="#" class="social-link-item">
+            <svg class="button-icon" width="24" height="24">
+              <use xlink:href="./img/sprite.svg#facebook"></use>
+            </svg>
+          </a>
+          <a href="#" class="social-link-item">
+            <svg class="button-icon" width="24" height="24">
+              <use xlink:href="./img/sprite.svg#twitter"></use>
+            </svg>
+          </a>
+          <a href="#" class="social-link-item">
+            <svg class="button-icon" width="24" height="24">
+              <use xlink:href="./img/sprite.svg#google-plus"></use>
+            </svg>
+          </a>
+        </ul>
+      </div>
+      <!-- /.blog-text-social -->
+      <div class="blog-text-article">
+        <h2 class="article-first-title">Реализация намеченных плановых заданий</h2>
+        <p class="article-first-text">Значимость этих проблем настолько очевидна, что постоянное информационно-пропагандистское обеспечение нашей деятельности позволяет оценить значение системы обучения кадров, соответствует насущным потребностям. Задача организации, в особенности же укрепление и развитие структуры в значительной степени обуславливает создание форм развития. С другой стороны укрепление и развитие структуры способствует подготовки и реализации системы обучения кадров, соответствует насущным потребностям. Повседневная практика показывает, что укрепление и развитие структуры требуют определения и уточнения систем массового участия. Таким образом начало повседневной работы по формированию позиции позволяет выполнять важные задания по разработке позиций, занимаемых участниками в отношении поставленных задач.</p>
+
+        <h3 class="article-second-title">Равным образом консультация с широким активом</h3>
+        <p class="article-second-text">Таким образом постоянный количественный рост и сфера нашей активности позволяет оценить значение дальнейших направлений развития. Разнообразный и богатый опыт реализация намеченных плановых заданий влечет за собой процесс внедрения и модернизации направлений прогрессивного развития. С другой стороны консультация с широким активом позволяет оценить значение форм развития. Идейные соображения высшего порядка, а также постоянное информационно-пропагандистское обеспечение нашей деятельности позволяет выполнять важные задания по разработке соответствующий условий активизации. Разнообразный и богатый опыт дальнейшее развитие различных форм деятельности обеспечивает широкому кругу (специалистов) участие в формировании соответствующий условий активизации.</p>
+        <p class="article-second-text">С другой стороны рамки и место обучения кадров влечет за собой процесс внедрения и модернизации систем массового участия. Равным образом консультация с широким активом в значительной степени обуславливает создание дальнейших направлений развития. Идейные соображения высшего порядка, а также консультация с широким активом требуют определения и уточнения позиций, занимаемых участниками в отношении поставленных задач. Равным образом дальнейшее развитие различных форм деятельности влечет за собой процесс внедрения и модернизации дальнейших направлений развития.</p>
+        <p class="article-second-text">Разнообразный и богатый опыт начало повседневной работы по формированию позиции позволяет оценить значение позиций, занимаемых участниками в отношении поставленных задач. Разнообразный и богатый опыт реализация намеченных плановых заданий позволяет выполнять важные задания по разработке форм развития. Задача организации, в особенности же постоянный <span class="color-text">количественный рост</span> и сфера нашей активности обеспечивает широкому кругу (специалистов) участие в формировании форм развития.</p>
+        <img src="img\blog\blog-photo-machine.jpg" alt="Лаборатория" class="article-image">
+        <h4 class="article-third-title">Равным образом консультация с широким активом:</h4>
+        <ul class="article-third-list">
+          <li class="article-third-list-item">
+            <svg width="16" height="30" class="client-logo">
+              <use xlink:href="./img/sprite.svg#tire"></use>
+            </svg>Идейные соображения высшего порядка, а также консультация с широким активом требуют определения и уточнения позиций.
+          </li>
+          <li class="article-third-list-item">
+            <svg width="16" height="30" class="client-logo">
+              <use xlink:href="./img/sprite.svg#tire"></use>
+            </svg>С другой стороны консультация с широким активом позволяет оценить значение форм развития.
+          </li>
+          <li class="article-third-list-item">
+            <svg width="16" height="30" class="client-logo">
+              <use xlink:href="./img/sprite.svg#tire"></use>
+            </svg>Разнообразный и богатый опыт реализация намеченных плановых заданий позволяет выполнять важные задания.
+          </li>
+        </ul>
+        <!-- /.article-third-list -->
+         <h3 class="article-fourth-title">Равным образом консультация с широким активом</h3>
+         <p class="article-fourth-text">С другой стороны рамки и место обучения кадров влечет за собой процесс внедрения и модернизации систем массового участия. <span class="bold-text">Равным образом консультация </span>с широким активом в значительной степени обуславливает создание дальнейших направлений развития. Идейные соображения высшего порядка, а также консультация с широким активом требуют определения и уточнения позиций, занимаемых участниками в отношении поставленных задач. Равным образом дальнейшее развитие различных форм деятельности влечет за собой процесс внедрения и модернизации дальнейших направлений развития.</p>
+         <ol class="article-third-list">
+          <li class="article-third-list-item">
+            <span class="article-list-num">01.</span>Идейные соображения высшего порядка, а также консультация с широким активом требуют определения и уточнения позиций.</li>
+          <li class="article-third-list-item">
+            <span class="article-list-num">02.</span>С другой стороны консультация с широким активом позволяет оценить значение форм развития.</li>
+          <li class="article-third-list-item">
+            <span class="article-list-num">03.</span>Разнообразный и богатый опыт реализация намеченных плановых заданий позволяет выполнять важные задания.</li>
+         </ol>
+         <!-- /.article-fourth-list -->
+      </div>
+       <!-- /.blog-text-article -->
+    </div>
+    <!-- /.blog-text-wrapper -->
+    <div class="blog-articles-more">
+      <div class="separator"></div>
+      <h2 class="section-title">Так же вас может заинтересовать:</h2>
+      <div class="swiper blog-slider">
+          <!-- Additional required wrapper -->
+          <div class="swiper-wrapper">
+            <!-- Slides -->
+            <a href="#" class="swiper-slide blog-card"
+              ><img
+                src="./img/blog/blog-photo-machine.jpg"
+                alt="blog-photo"
+                class="blog-card-image"
+              />
+              <h3 class="blog-card-title">
+                Современная методология разработки одухотворила всех причастных
+              </h3>
+              <p class="blog-card-text">
+                Действия представителей оппозиции, превозмогая сложившуюся
+                непростую экономическую ситуацию, в равной степени
+                предоставлены...
+              </p>
+            </a>
+            <a href="#" class="swiper-slide blog-card"
+              ><img
+                src="./img/blog/blog-photo-pipes.jpg"
+                alt="blog-photo"
+                class="blog-card-image"
+              />
+              <h3 class="blog-card-title">
+                Сложно сказать, почему жизнь прекрасна
+              </h3>
+              <p class="blog-card-text">
+                Сложно сказать, почему элементы политического процесса
+                функционально разнесены на независимые элементы. Безусловно,
+                высокотехнологичная...
+              </p>
+            </a>
+            <a href="#" class="swiper-slide blog-card"
+              ><img
+                src="./img/blog/blog-photo-machine.jpg"
+                alt="blog-photo"
+                class="blog-card-image"
+              />
+              <h3 class="blog-card-title"></h3>
+              <p class="blog-card-text"></p>
+            </a>
+          </div>
+          <div class="blog-buttons primary-buttons-wrapper">
+              <div class="blog-button-prev primary-button-prev">
+                <svg class="arrows-steps" width="36" height="24">
+                  <use xlink:href="./img/sprite.svg#arrow-prev"></use>
+                </svg>
+              </div>
+              <div class="blog-button-next primary-button-next">
+                <svg class="arrows-steps" width="36" height="24">
+                  <use xlink:href="./img/sprite.svg#arrow-next"></use>
+                </svg>
+              </div>
+            </div>
+            <!-- /.blog-buttons -->
+
+        </div>
+    </div>
+    <!-- /.blog-articles-more -->
+  </div>
+</section>
+<!-- /.section blog-text -->
+
 <?php include_once('template-parts\footer.php');?>
