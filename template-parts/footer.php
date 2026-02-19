5563df8a34544e3268e655a9d8419bb2830f1829
aleksi2105 <aleksi2105@yandex.ru>
2026-02-20 02:04:20 +0300

    add: favicon

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -261,5 +261,6 @@
     <script src="./js/swiper-bundle.min.js"></script>
     <script src="./js/just-validate.production.min.js"></script>
     <script src="./js/main.js"></script>
+    
   </body>
 </html>
