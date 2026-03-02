abf317fb721b5ee37e22ccdfb43719e15ed48c6b
aleksi2105 <aleksi2105@yandex.ru>
2026-03-03 02:38:28 +0300

    fix: avtohim page

--- avtohim.php
+++ avtohim.php
@@ -9,7 +9,7 @@ $aboutTitle = 'Контрактное производство <br> автомо
 $description = 'Прежде всего, начало повседневной работы по формированию позиции однозначно фиксирует необходимость приоритизации разума над эмоциями. Кстати, представители современных социальных резервов неоднозначны и будут описаны максимально подробно. <br> <br>
 Но социально-экономическое развитие напрямую зависит от поставленных обществом задач! Безусловно, постоянный количественный рост и сфера нашей активности предоставляет широкие возможности для системы обучения кадров, соответствующей насущным потребностям. В целом, конечно, выбранный нами инновационный путь обеспечивает широкому кругу (специалистов) участие в формировании своевременного выполнения сверхзадачи. А ещё элементы политического процесса представлены в исключительно положительном свете. Высокий уровень вовлечения представителей целевой аудитории является четким доказательством простого факта: высокотехнологичная концепция общественного уклада создаёт предпосылки для глубокомысленных рассуждений. В своём стремлении повысить качество жизни, они забывают, что дальнейшее развитие различных форм деятельности, а также свежий взгляд на привычные вещи - безусловно открывает новые горизонты для поставленных обществом задач.';
 include_once('./template-parts/header-page.php');?>
-<?php include_once('./template-parts/about-image-text.php')?>
+<section class="avtohim-about"><?php include_once('./template-parts/about-image-text.php')?></section>
 <!-- /.section section-about -->
  <section class="section founder avtohim">
   <div class="container production-wrapper">
@@ -20,49 +20,49 @@ include_once('./template-parts/header-page.php');?>
       <div class="clients-content">
        <ul class="clients-list">
               <li class="clients-list-item">
-                <svg width="30" height="30" class="client-logo">
+                <svg width="30" height="30" class="avtohim-client-logo">
                   <use xlink:href="./img/sprite.svg#Himic"></use>
                 </svg>
                 Полное описание товаров
               </li>
               <li class="clients-list-item">
-                <svg width="30" height="30" class="client-logo">
+                <svg width="30" height="30" class="avtohim-client-car">
                   <use xlink:href="./img/sprite.svg#carwash"></use>
                 </svg>
                 Актуальные цены
               </li>
               <li class="clients-list-item">
-                <svg width="30" height="30" class="clients-list-icon">
+                <svg width="30" height="30" class="avtohim-client-logo">
                   <use xlink:href="./img/sprite.svg#Himic"></use>
                 </svg>
                 Полное описание товаров
               </li>
               <li class="clients-list-item">
-                <svg width="30" height="30" class="clients-list-icon">
+                <svg width="30" height="30" class="avtohim-client-car">
                   <use xlink:href="./img/sprite.svg#carwash"></use>
                 </svg>
                 Актуальные цены
               </li>
               <li class="clients-list-item">
-                <svg width="30" height="30" class="clients-list-icon">
+                <svg width="30" height="30" class="avtohim-client-brush">
                   <use xlink:href="./img/sprite.svg#brush"></use>
                 </svg>
                 Лаки и краски
               </li>
               <li class="clients-list-item">
-                <svg width="30" height="30" class="clients-list-icon">
+                <svg width="30" height="30" class="avtohim-client-logo">
                   <use xlink:href="./img/sprite.svg#Himic"></use>
                 </svg>
                 Химические производства
               </li>
               <li class="clients-list-item">
-                <svg width="30" height="30" class="clients-list-icon">
+                <svg width="30" height="30" class="avtohim-client-brush">
                   <use xlink:href="./img/sprite.svg#brush"></use>
                 </svg>
                 Лаки и краски
               </li>
               <li class="clients-list-item">
-                <svg width="30" height="30" class="clients-list-icon">
+                <svg width="30" height="30" class="avtohim-client-logo">
                   <use xlink:href="./img/sprite.svg#Himic"></use>
                 </svg>
                 Химические производства
