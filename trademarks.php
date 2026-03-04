a511dfefdfc3f6cee935db764a189b4d5f4bd2f7
aleksi2105 <aleksi2105@yandex.ru>
2026-03-04 03:33:56 +0300

    fix: trademarks page Pixel Perf

--- trademarks.php
+++ trademarks.php
@@ -4,7 +4,7 @@ $header_style = 'header-bg-grey';
 $showTitle = true;
 $showCta = true;
 include_once('./template-parts/header-page.php');?>
-<section class="section own-brands">
+<section class="section own-brands trademarks-page">
   <?php
   include_once('./template-parts/trademarks-block.php');?>
 </section>
